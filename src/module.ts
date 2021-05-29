d2d1beb1dd4bfad4298855a755687d30025f4683
Simon Holloway <simon@syhol.io>
2021-05-29 22:17:21 +0100

    refactor: use the ioc callables

--- src/module.ts
+++ src/module.ts
@@ -1,33 +1,33 @@
 import {
   bind,
   Definition,
-  emitUnknownValue,
   EventSink,
   on,
   RegisterDefinitions,
   ServiceLocator,
   Shutdown,
   Startup,
-  Method,
   AbstractClass,
-  AllInstanceType,
-  Newable,
+  Callable,
+  callable,
 } from '@badbury/ioc';
 import { Timer, TimerController, DurationFunction } from './timer-controller';
 
-export abstract class TimerDefinition<A extends AbstractClass[] = []>
+export class TimerDefinition<A extends AbstractClass[] = []>
   implements Definition<TimerDefinition> {
   definition = TimerDefinition;
-  constructor(public readonly timer: Timer) {}
-  abstract register(resolver: ServiceLocator, controller: TimerController, sink: EventSink): void;
-  abstract emit(): TimerDefinition<A>;
-}
 
-type FunctionOf<
-  TArgsType extends AbstractClass[],
-  TReturn = void,
-  TArgs extends unknown[] = AllInstanceType<TArgsType>
-> = (...args: TArgs) => TReturn;
+  constructor(public readonly timer: Timer, private readonly callable: Callable) {}
+
+  register(resolver: ServiceLocator, controller: TimerController, sink: EventSink): void {
+    const timer = this.timer.do(() => this.callable.handle([], resolver, sink));
+    controller.use(timer);
+  }
+
+  emit(): TimerDefinition<A> {
+    return new TimerDefinition(this.timer, this.callable.emit());
+  }
+}
 
 export class TimerDefinitionBuilder<A extends AbstractClass[] = []> {
   constructor(public readonly timer: Timer, public args: A = ([] as unknown) as A) {}
@@ -45,80 +45,7 @@ export class TimerDefinitionBuilder<A extends AbstractClass[] = []> {
     return new TimerDefinitionBuilder(this.timer, args);
   }
 
-  do(target: FunctionOf<A>): TimerDefinition;
-  do<C extends Newable, M extends Method<C, A>>(target: C, method: M): TimerDefinition;
-  do<C extends Newable, M extends Method<C, A>>(
-    target: C | FunctionOf<A>,
-    method?: M,
-  ): TimerDefinition {
-    return method
-      ? new ClassTimerDefinition(this.timer, this.args, target as C, method)
-      : new FunctionTimerDefinition(this.timer, this.args, target as FunctionOf<A>);
-  }
-}
-
-export class ClassTimerDefinition<
-  A extends AbstractClass[],
-  V extends Newable,
-  M extends Method<V, A>
-> extends TimerDefinition {
-  constructor(
-    timer: Timer,
-    public args: A,
-    private listenerClass: V,
-    private listenerMethod: M,
-    private shouldEmitResponse: boolean = false,
-  ) {
-    super(timer);
-  }
-
-  register(resolver: ServiceLocator, controller: TimerController, sink: EventSink): void {
-    const args = this.args.map((key) => resolver.get(key)) as AllInstanceType<A>;
-    const handler = resolver.get(this.listenerClass);
-    const timer = this.timer.do(() => {
-      const result = handler[this.listenerMethod](...args);
-      if (this.shouldEmitResponse) {
-        emitUnknownValue(result, sink);
-      }
-    });
-    controller.use(timer);
-  }
-
-  emit(): ClassTimerDefinition<A, V, M> {
-    return new ClassTimerDefinition(
-      this.timer,
-      this.args,
-      this.listenerClass,
-      this.listenerMethod,
-      true,
-    );
-  }
-}
-
-export class FunctionTimerDefinition<A extends AbstractClass[]> extends TimerDefinition {
-  constructor(
-    timer: Timer,
-    public args: A,
-    private handler: FunctionOf<A>,
-    private shouldEmitResponse: boolean = false,
-  ) {
-    super(timer);
-  }
-
-  register(resolver: ServiceLocator, controller: TimerController, sink: EventSink): void {
-    const args = this.args.map((key) => resolver.get(key)) as AllInstanceType<A>;
-    const timer = this.timer.do(() => {
-      const result = this.handler(...args);
-      if (this.shouldEmitResponse) {
-        emitUnknownValue(result, sink);
-      }
-    });
-    controller.use(timer);
-  }
-
-  emit(): FunctionTimerDefinition<A> {
-    return new FunctionTimerDefinition(this.timer, this.args, this.handler, true);
-  }
+  do = callable<[], A>(this.args).map((callable) => new TimerDefinition(this.timer, callable));
 }
 
 export class TimerModule {
